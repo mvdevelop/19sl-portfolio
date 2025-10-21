7455543c473c9acd08a2cb711e2abdff483b045b
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-20 22:11:51 -0300

    starting repository

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <h1 className=''>Hello, World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App;
